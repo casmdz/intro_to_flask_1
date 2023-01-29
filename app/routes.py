db45d95386a48532b08e717ecd64574766831dc3
casmdz <t4mendez@gmail.com>
2023-01-29 17:12:04 -0600

    tuesday updated very basic files

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,17 @@
 from app import app
 from flask import render_template
+import os
 
 @app.route('/')
 def homePage():
     people = ['shoha', 'brandt', 'aubrey', 'and me']
     text = 'SENDING THIS FROM PYTHON'
-    favorite_txt = 'Be sure to check out my top 5 artists'
-    return render_template('index.html', people = people, my_text = text, favorite_txt = favorite_txt) 
-
-#doesnt have to be "index" #sending the variable "people" 
-# #you can access the KEY "TEXT" in index, people = people, my_test = text
-
-# @app.route('/favorite_5')
-# def favorite_5():
-#     favorites = ['Tyler the Creator', 'Men I Trust', 'Porcupine Tree', 'The Garden', 'TOKYOPILL']
-#     return render_template('favorite_5.html', favorites = favorites) 
-
+    return render_template('index.html', people = people, my_text = text) 
 
 @app.route('/contact')
 def contactPage():
-    okay = 'placeholder'
-    return render_template('contact.html', okay_text = okay)
+    return render_template('contact.html')
+
+@app.route('/about')
+def aboutPage():
+    return render_template('about.html')
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__name__)) #path finds what file we're in
+basedir = os.path.abspath(os.path.dirname(__name__)) 
+
+#path finds what file we're in
 #being super specific... whatever __name__ is in, *that is the path that we want*
 
 class Config():
@@ -8,3 +10,6 @@ class Config():
     # FLASK_ENV = 'development' #how to get to your env. by default it's "production".
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
+    # SECRET_KEY = os.environ.get("SECRET_KEY")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False

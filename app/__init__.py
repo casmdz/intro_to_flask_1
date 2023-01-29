0789d8bcb1a9d610acaffdfd6ace4d3fcf571865
casmdz <t4mendez@gmail.com>
2023-01-29 08:34:18 -0600

    w5d1 monday

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,15 @@
 
 from flask import Flask
 from config import Config #lowkey we could've put the code in this file, but shoha likes less clutter
-
+from flask import render_template
 
 app = Flask(__name__)
 app.config.from_object(Config) #from this set all my settings
 
 from . import routes
+
+
+@app.route('/favorite_5')
+def favorite_5():
+    favorites = ['Tyler the Creator', 'Men I Trust', 'Porcupine Tree', 'The Garden', 'TOKYOPILL']
+    return render_template('favorite_5.html', favorites = favorites) 
--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,19 @@ from flask import render_template
 def homePage():
     people = ['shoha', 'brandt', 'aubrey', 'and me']
     text = 'SENDING THIS FROM PYTHON'
-    return render_template('index.html', people = people, my_text = text) 
+    favorite_txt = 'Be sure to check out my top 5 artists'
+    return render_template('index.html', people = people, my_text = text, favorite_txt = favorite_txt) 
 
 #doesnt have to be "index" #sending the variable "people" 
 # #you can access the KEY "TEXT" in index, people = people, my_test = text
 
+# @app.route('/favorite_5')
+# def favorite_5():
+#     favorites = ['Tyler the Creator', 'Men I Trust', 'Porcupine Tree', 'The Garden', 'TOKYOPILL']
+#     return render_template('favorite_5.html', favorites = favorites) 
+
+
 @app.route('/contact')
 def contactPage():
-    return render_template('contact.html')
+    okay = 'placeholder'
+    return render_template('contact.html', okay_text = okay)
